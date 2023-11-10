e2318ad9f589699372b57589e6e7ce0dbcf8a869
BuseNurAydn <83844222+BuseNurAydn@users.noreply.github.com>
2023-11-10 14:49:28 +0300

    Initial commit

---
+++ src/ExceptionDemo.java
@@ -0,0 +1,30 @@
+
+public class ExceptionDemo {
+	
+
+	   private static void multipleDivision(int[] numbers, int[] dividers) {
+	    if (numbers.length != dividers.length) {
+	        System.out.println("Array sizes are not equal");
+	        throw new IllegalArgumentException("Array sizes must be equal");
+	    }
+	    for (int i = 0; i < numbers.length; i++) {
+	        try {
+	            System.out.println(numbers[i] + "/" + dividers[i] + "=" + numbers[i] / dividers[i]);
+	        } catch (ArithmeticException ex) {
+	            System.out.println("Division by 0, " + numbers[i] + "/" + dividers[i]);
+	        }
+	    }
+	    System.out.println("Program ended successfully");
+	}
+
+	public static void main(String[] args) {
+	    try {
+        int[] numbers = new int[]{1, 34, 56, 23, 78, 123, 49};
+        int[] dividers = new int[]{5, 0, 12, 0, 34};
+        multipleDivision(numbers, dividers);
+    } catch (RuntimeException ex) {
+        System.out.println("Fatal error, program terminated");
+    }
+}
+
+}
---
+++ src/ExceptionDemo7.java
@@ -0,0 +1,33 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ExceptionDemo7 {
+
+	public static void main(String[] args) throws IOException {
+		// throws
+		   FileInputStream input = null;
+		      
+	        try {
+	            ClassLoader classLoader = ExceptionDemo7.class.getClassLoader();
+	            input = new FileInputStream(classLoader.getResource("file.txt").getFile());
+	            int data = input.read();
+	          
+	            while (data != -1) {
+	                System.out.print((char) data);
+	                data = input.read();
+	            }
+	        } finally {
+	            if (input != null) {
+	                try {
+	                    input.close();
+	                } catch (IOException e) {
+	                    System.out.println("Exception occurred while closing file");
+	                }
+	            }
+	        }
+	    }
+	
+
+	
+
+}
